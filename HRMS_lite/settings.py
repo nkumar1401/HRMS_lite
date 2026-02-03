dcb46de1cb8fbf90b7a8c3348e8d9956278a9bfe
Nirmal Kumar Bhagatkar <nkbhagatkar@gmail.com>
2026-02-03 13:44:52 +0530

    Fix: Add Render host to ALLOWED_HOSTS

--- HRMS_lite/settings.py
+++ HRMS_lite/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-_1!f2@+ctv^1&w3g!4*@#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,.onrender.com', cast=lambda v: [s.strip() for s in v.split(',') if s.strip()])
 
 
 # Application definition
