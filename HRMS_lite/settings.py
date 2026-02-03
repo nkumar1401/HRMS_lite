5b3b6d87049f4f7c657ac1e5fcd21d1c52e2da10
Nirmal Kumar Bhagatkar <nkbhagatkar@gmail.com>
2026-02-03 14:09:39 +0530

    Migrate to PostgreSQL for Render deployment

--- HRMS_lite/settings.py
+++ HRMS_lite/settings.py
@@ -68,19 +68,17 @@ WSGI_APPLICATION = 'HRMS_lite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# Database
+# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME', default='HRMS_lite'),
-        'USER': config('DB_USER', default='root'),
-        'PASSWORD': config('DB_PASSWORD', default='root'),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='3306'),
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
-    }
+    'default': config(
+        'DATABASE_URL',
+        default=f"postgresql://{config('DB_USER', default='postgres')}:{config('DB_PASSWORD', default='')}@{config('DB_HOST', default='localhost')}:{config('DB_PORT', default='5432')}/{config('DB_NAME', default='HRMS_lite')}",
+        cast=dj_database_url.parse
+    )
 }
 
 
